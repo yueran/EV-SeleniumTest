0fad9853e09a44955cb352dc324c595ebef260c4
yueran <yueran@zignage.com>
2012-03-09 18:04:47 -0500

    Office-change the structure for classify products

--- WebDriver/Webdriver/testCase/functionalTesting/ClassifyProduct/en_ClassifyProducts_Function_DragAndDrop.py
+++ WebDriver/Webdriver/testCase/functionalTesting/ClassifyProduct/en_ClassifyProducts_Function_DragAndDrop.py
@@ -1,6 +1,11 @@
 #This one works, but not fully verified.
 from Webdriver.all_globals import *
-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver import ActionChains
+import unittest, time, re
 
 class EnClassifyProductsFunctionDragAndDrop(unittest.TestCase):
     def setUp(self):
