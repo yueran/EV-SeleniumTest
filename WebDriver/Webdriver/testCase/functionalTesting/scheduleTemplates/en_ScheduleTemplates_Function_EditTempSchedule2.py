9d89246fd1fe2ea56182f84202bd0b129577e672
yueran <yueran@zignage.com>
2012-03-23 15:34:47 -0400

    office-scheduleTemp

--- WebDriver/Webdriver/testCase/functionalTesting/scheduleTemplates/en_ScheduleTemplates_Function_EditTempSchedule2.py
+++ WebDriver/Webdriver/testCase/functionalTesting/scheduleTemplates/en_ScheduleTemplates_Function_EditTempSchedule2.py
@@ -3,34 +3,91 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 import unittest, time, re
+from Webdriver.all_globals import *
 
 class EnScheduleTemplatesFunctionEditTempSchedule2(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(30)
-        self.base_url = "http://192.168.1.204:8080/"
-        self.verificationErrors = []
-    
+        gb_setUp(self)
+ #Note the test is time sensitive. Need to adjust the testing time as time goes by.
     def test_en_schedule_templates_function_edit_temp_schedule2(self):
         driver = self.driver
+        gb_login(self)
         # Test Case not completed.
-        try: self.assertEqual("03/01/2012", driver.find_element_by_id("scheduleCol_schedule193startDate").get_attribute("value"))
+        driver = self.driver
+        gb_login(self)
+        driver.get(self.base_url + "/ev/scheduletemplates")
+        try: self.assertIn(u"EditTempSchedule2", driver.find_element_by_class_name("templateColumn").text)
         except AssertionError as e: self.verificationErrors.append(str(e))
+        try: self.assertTrue(self.is_element_present(By.CSS_SELECTOR,"#scheduleCol_schedule919 > div.itemContent.templateBg > span.fold"))
+        except AssertionError as e: self.verificationErrors.append(str(e))
+        try: self.assertTrue(self.is_element_present(By.ID,"scheduleCol_schedule919Options"))
+        except AssertionError as e: self.verificationErrors.append(str(e))
+        driver.find_element_by_css_selector("#scheduleCol_schedule919 > div.itemContent.templateBg > span.fold").click()
+
+        for i in range(60):
+            try:
+                if u"Start Date:" == driver.find_element_by_css_selector("#scheduleCol_schedule919 > div.scheduleInfo > span.dateSettings > label > span").text: break
+            except: pass
+            time.sleep(1)
+        else: self.fail("time out")
+        try: self.assertEqual("04/01/2012", driver.find_element_by_id("scheduleCol_schedule919startDate").get_attribute("value"))
+        except AssertionError as e: self.verificationErrors.append(str(e))
+        try: self.assertTrue(self.is_element_present(By.ID,"scheduleCol_schedule919startDate"))
+        except AssertionError as e: self.verificationErrors.append(str(e))
+        driver.find_element_by_id("scheduleCol_schedule919startDate").click()
         driver.find_element_by_link_text("5").click()
-        try: self.assertEqual("03/05/2012", driver.find_element_by_id("scheduleCol_schedule193startDate").get_attribute("value"))
+        for i in range(60):
+            try:
+                if u"04/05/2012" == driver.find_element_by_id("scheduleCol_schedule919startDate").get_attribute("value"): break
+            except: pass
+            time.sleep(1)
+        else: self.fail("time out")
+        try: self.assertEqual("04/05/2012", driver.find_element_by_id("scheduleCol_schedule919startDate").get_attribute("value"))
         except AssertionError as e: self.verificationErrors.append(str(e))
-        try: self.assertEqual("03/30/2012", driver.find_element_by_id("scheduleCol_schedule193endDate").get_attribute("value"))
+        driver.find_element_by_id("scheduleCol_schedule919startDate").click()
+        driver.find_element_by_link_text("1").click()
+        for i in range(60):
+            try:
+                if u"04/01/2012" == driver.find_element_by_id("scheduleCol_schedule919startDate").get_attribute("value"): break
+            except: pass
+            time.sleep(1)
+        else: self.fail("time out")
+        try: self.assertEqual("04/01/2012", driver.find_element_by_id("scheduleCol_schedule919startDate").get_attribute("value"))
         except AssertionError as e: self.verificationErrors.append(str(e))
+
+        try: self.assertEqual("07/31/2012", driver.find_element_by_id("scheduleCol_schedule919endDate").get_attribute("value"))
+        except AssertionError as e: self.verificationErrors.append(str(e))
+
+        try: self.assertTrue(self.is_element_present(By.ID,"scheduleCol_schedule919endDate"))
+        except AssertionError as e: self.verificationErrors.append(str(e))
+        driver.find_element_by_id("scheduleCol_schedule919endDate").click()
         driver.find_element_by_link_text("25").click()
-        try: self.assertEqual("03/25/2012", driver.find_element_by_id("scheduleCol_schedule193endDate").get_attribute("value"))
+        for i in range(60):
+            try:
+                if u"07/25/2012" == driver.find_element_by_id("scheduleCol_schedule919endDate").get_attribute("value"): break
+            except: pass
+            time.sleep(1)
+        else: self.fail("time out")
+        try: self.assertEqual("07/25/2012", driver.find_element_by_id("scheduleCol_schedule919endDate").get_attribute("value"))
         except AssertionError as e: self.verificationErrors.append(str(e))
-        try: self.assertEqual("00:00:00", driver.find_element_by_id("scheduleCol_schedule193startTime").get_attribute("value"))
+        driver.find_element_by_id("scheduleCol_schedule919endDate").click()
+        driver.find_element_by_link_text("31").click()
+        for i in range(60):
+            try:
+                if u"07/31/2012" == driver.find_element_by_id("scheduleCol_schedule919endDate").get_attribute("value"): break
+            except: pass
+            time.sleep(1)
+        else: self.fail("time out")
+        try: self.assertEqual("07/31/2012", driver.find_element_by_id("scheduleCol_schedule919endDate").get_attribute("value"))
         except AssertionError as e: self.verificationErrors.append(str(e))
-        try: self.assertEqual("23:00:00", driver.find_element_by_id("scheduleCol_schedule193endTime").get_attribute("value"))
-        except AssertionError as e: self.verificationErrors.append(str(e))
-        try: self.assertTrue(self.is_element_present(By.ID, "scheduleCol_schedule193startTime"))
-        except AssertionError as e: self.verificationErrors.append(str(e))
-        driver.find_element_by_id("scheduleCol_schedule193startTime").click()
+
+
+        #try: self.assertEqual("00:00:00", driver.find_element_by_id("scheduleCol_schedule919startTime").get_attribute("value"))
+        #except AssertionError as e: self.verificationErrors.append(str(e))
+        #try: self.assertEqual("23:00:00", driver.find_element_by_id("scheduleCol_schedule919endTime").get_attribute("value"))
+        #except AssertionError as e: self.verificationErrors.append(str(e))
+
+        #driver.find_element_by_id("scheduleCol_schedule919startTime").click()
         # ERROR: Caught exception [ERROR: Unsupported command [typeKeys]]
     
     def is_element_present(self, how, what):
