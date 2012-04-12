ffb3bc7c1f2c89e57bf8b30daa11e00fac9ad78b
yueran <yueran@zignage.com>
2012-04-12 18:18:44 -0400

    office -- add create loops

---
+++ WebDriver/Webdriver/testCase/functionalTesting/scheduleTemplates/en_ScheduleTemplates_Function_EditTempSchedule2_1.py
@@ -0,0 +1,123 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+import unittest, time, re
+from Webdriver.all_globals import *
+
+class EnScheduleTemplatesFunctionEditTempSchedule2(unittest.TestCase):
+    def setUp(self):
+        gb_setUp(self)
+ #Note the test is time sensitive. Need to adjust the testing time as time goes by.
+ #Test the start date and end date.
+    def test_en_schedule_templates_function_edit_temp_schedule2(self):
+        driver = self.driver
+        gb_login(self)
+        # Test Case not completed.
+        driver = self.driver
+        gb_login(self)
+        driver.get(self.base_url + "/ev/scheduletemplates")
+        try: self.assertIn(u"EditTempSchedule2", driver.find_element_by_class_name("scheduleColumn").text)
+        except AssertionError as e: self.verificationErrors.append(str(e))
+        try: self.assertTrue(self.is_element_present(By.CSS_SELECTOR,"#scheduleCol_schedule919 > div.itemContent.templateBg > span.fold"))
+        except AssertionError as e: self.verificationErrors.append(str(e))
+        try: self.assertTrue(self.is_element_present(By.ID,"scheduleCol_schedule919Options"))
+        except AssertionError as e: self.verificationErrors.append(str(e))
+        driver.find_element_by_css_selector("#scheduleCol_schedule919 > div.itemContent.templateBg > span.fold").click()
+
+        for i in range(60):
+            try:
+                if u"Start Date:" == driver.find_element_by_css_selector("#scheduleCol_schedule919 > div.scheduleInfo > span.dateSettings > label > span").text: break
+            except: pass
+            time.sleep(1)
+        else: self.fail("time out")
+        try: self.assertEqual("00:00:00", driver.find_element_by_id("scheduleCol_schedule919startTime").get_attribute("value"))
+        except AssertionError as e: self.verificationErrors.append(str(e))
+        try: self.assertTrue(self.is_element_present(By.ID,"scheduleCol_schedule919startTime"))
+        except AssertionError as e: self.verificationErrors.append(str(e))
+        driver.find_element_by_id("scheduleCol_schedule919startDate").click()
+        driver.find_element_by_link_text("5").click()
+        for i in range(60):
+            try:
+                if u"04/05/2012" == driver.find_element_by_id("scheduleCol_schedule919startDate").get_attribute("value"): break
+            except: pass
+            time.sleep(1)
+        else: self.fail("time out")
+        try: self.assertEqual("04/05/2012", driver.find_element_by_id("scheduleCol_schedule919startDate").get_attribute("value"))
+        except AssertionError as e: self.verificationErrors.append(str(e))
+		
+        driver.refresh()
+        driver.find_element_by_css_selector("#scheduleCol_schedule919 > div.itemContent.templateBg > span.fold").click()
+        for i in range(60):
+            try:
+                if u"Start Date:" == driver.find_element_by_css_selector("#scheduleCol_schedule919 > div.scheduleInfo > span.dateSettings > label > span").text: break
+            except: pass
+            time.sleep(1)
+        else: self.fail("time out")
+        driver.find_element_by_id("scheduleCol_schedule919startDate").click()
+        driver.find_element_by_link_text("1").click()
+        for i in range(60):
+            try:
+                if u"04/01/2012" == driver.find_element_by_id("scheduleCol_schedule919startDate").get_attribute("value"): break
+            except: pass
+            time.sleep(1)
+        else: self.fail("time out")
+        try: self.assertEqual("04/01/2012", driver.find_element_by_id("scheduleCol_schedule919startDate").get_attribute("value"))
+        except AssertionError as e: self.verificationErrors.append(str(e))
+
+        try: self.assertEqual("07/31/2012", driver.find_element_by_id("scheduleCol_schedule919endDate").get_attribute("value"))
+        except AssertionError as e: self.verificationErrors.append(str(e))
+
+        try: self.assertTrue(self.is_element_present(By.ID,"scheduleCol_schedule919endDate"))
+        except AssertionError as e: self.verificationErrors.append(str(e))
+        driver.find_element_by_id("scheduleCol_schedule919endDate").click()
+        driver.find_element_by_link_text("25").click()
+        for i in range(60):
+            try:
+                if u"07/25/2012" == driver.find_element_by_id("scheduleCol_schedule919endDate").get_attribute("value"): break
+            except: pass
+            time.sleep(1)
+        else: self.fail("time out")
+        try: self.assertEqual("07/25/2012", driver.find_element_by_id("scheduleCol_schedule919endDate").get_attribute("value"))
+        except AssertionError as e: self.verificationErrors.append(str(e))
+
+        driver.refresh()
+        driver.find_element_by_css_selector("#scheduleCol_schedule919 > div.itemContent.templateBg > span.fold").click()
+        for i in range(60):
+            try:
+                if u"End Date:" == driver.find_element_by_xpath("//li[@id='scheduleCol_schedule919']/div[2]/span/label[2]/span").text: break
+            except: pass
+            time.sleep(1)
+        else: self.fail("time out")
+        driver.find_element_by_id("scheduleCol_schedule919endDate").click()
+        driver.find_element_by_link_text("31").click()
+        for i in range(60):
+            try:
+                if u"07/31/2012" == driver.find_element_by_id("scheduleCol_schedule919endDate").get_attribute("value"): break
+            except: pass
+            time.sleep(1)
+        else: self.fail("time out")
+
+        try: self.assertEqual("07/31/2012", driver.find_element_by_id("scheduleCol_schedule919endDate").get_attribute("value"))
+        except AssertionError as e: self.verificationErrors.append(str(e))
+
+
+        #try: self.assertEqual("00:00:00", driver.find_element_by_id("scheduleCol_schedule919startTime").get_attribute("value"))
+        #except AssertionError as e: self.verificationErrors.append(str(e))
+        #try: self.assertEqual("23:00:00", driver.find_element_by_id("scheduleCol_schedule919endTime").get_attribute("value"))
+        #except AssertionError as e: self.verificationErrors.append(str(e))
+
+        #driver.find_element_by_id("scheduleCol_schedule919startTime").click()
+        # ERROR: Caught exception [ERROR: Unsupported command [typeKeys]]
+    
+    def is_element_present(self, how, what):
+        try: self.driver.find_element(by=how, value=what)
+        except NoSuchElementException, e: return False
+        return True
+    
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ WebDriver/Webdriver/testSuites/en_ScheduleTemplates.py
@@ -0,0 +1,85 @@
+#Issues that been tested:
+#1)Content verification:
+#2)EditTemplateName
+#3)EditTemplateSchedule(Partically)
+#4)Search function.
+#5)Note: Drag and Drop function testing is pending.
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+import unittest, time, re
+import HTMLTestRunner
+from Webdriver.all_globals import *
+from Webdriver.testCase.functionalTesting.scheduleTemplates import en_ScheduleTemplates_ContentVerification as TestCase1
+from Webdriver.testCase.functionalTesting.scheduleTemplates import en_ScheduleTemplates_ContentVerification_Help as TestCase2
+from Webdriver.testCase.functionalTesting.scheduleTemplates import en_ScheduleTemplates_Function_DragAndDrop as TestCase3
+from Webdriver.testCase.functionalTesting.scheduleTemplates import en_ScheduleTemplates_Function_EditTempName as TestCase4
+from Webdriver.testCase.functionalTesting.scheduleTemplates import en_ScheduleTemplates_Function_EditTempSchedule as TestCase5
+from Webdriver.testCase.functionalTesting.scheduleTemplates import en_ScheduleTemplates_Function_EditTempSchedule2 as TestCase6
+from Webdriver.testCase.functionalTesting.scheduleTemplates import en_ScheduleTemplates_Function_Search as TestCase7
+
+def setUp(self):
+    gb_setUp(self)
+	
+class EnScheduleTemplatesContentVerification(TestCase1.EnScheduleTemplatesContentVerification):
+    
+    def test_en_schedule_templates_content_verification(self):
+		TestCase1.EnScheduleTemplatesContentVerification.test_en_schedule_templates_content_verification(self)
+
+class EnScheduleTemplatesContentVerificationHelp(TestCase2.EnScheduleTemplatesContentVerificationHelp):
+    def test_en_schedule_templates_content_verification_help(self):
+		TestCase2.EnScheduleTemplatesContentVerificationHelp.test_en_schedule_templates_content_verification_help(self)
+
+#Drag and drop not implemented yet.
+class EnScheduleTemplatesFunctionDragAndDrop(TestCase3.EnScheduleTemplatesFunctionDragAndDrop):
+	def test_en_schedule_templates_function_drag_and_drop(self):
+		TestCase3.EnScheduleTemplatesFunctionDragAndDrop.test_en_schedule_templates_function_drag_and_drop(self)
+		
+class EnScheduleTemplatesFunctionEditTempName(TestCase4.EnScheduleTemplatesFunctionEditTempName):
+	def test_en_schedule_templates_function_edit_temp_name(self):
+		TestCase4.EnScheduleTemplatesFunctionEditTempName.test_en_schedule_templates_function_edit_temp_name(self)
+
+class EnScheduleTemplatesFunctionEditTempSchedule1(TestCase5.EnScheduleTemplatesFunctionEditTempSchedule1):
+    def test_en_schedule_templates_function_edit_temp_schedule1(self):
+		TestCase5.EnScheduleTemplatesFunctionEditTempSchedule1.test_en_schedule_templates_function_edit_temp_schedule1(self)
+
+class EnScheduleTemplatesFunctionEditTempSchedule2(TestCase6.EnScheduleTemplatesFunctionEditTempSchedule2):
+	def test_en_schedule_templates_function_edit_temp_schedule2(self):
+		TestCase6.EnScheduleTemplatesFunctionEditTempSchedule2.test_en_schedule_templates_function_edit_temp_schedule2(self)
+
+class EnScheduleTemplatesFunctionSearch(TestCase7.EnScheduleTemplatesFunctionSearch):
+	def test_en_schedule_templates_function_search(self):
+		TestCase7.EnScheduleTemplatesFunctionSearch.test_en_schedule_templates_function_search(self)
+
+
+def is_element_present(self, how, what):
+	try: self.driver.find_element(by=how, value=what)
+	except NoSuchElementException, e: return False
+	return True
+    
+def tearDown(self):
+	self.driver.quit()
+	self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+    testsuite = unittest.TestSuite()
+
+    testsuite.addTest(EnScheduleTemplatesContentVerification("test_en_schedule_templates_content_verification"))
+    testsuite.addTest(EnScheduleTemplatesContentVerificationHelp("test_en_schedule_templates_content_verification_help"))
+    testsuite.addTest(EnScheduleTemplatesFunctionDragAndDrop("test_en_schedule_templates_function_drag_and_drop"))
+    testsuite.addTest(EnScheduleTemplatesFunctionEditTempName("test_en_schedule_templates_function_edit_temp_name"))
+    testsuite.addTest(EnScheduleTemplatesFunctionEditTempSchedule1("test_en_schedule_templates_function_edit_temp_schedule1"))
+    testsuite.addTest(EnScheduleTemplatesFunctionEditTempSchedule2("test_en_schedule_templates_function_edit_temp_schedule2"))
+    testsuite.addTest(EnScheduleTemplatesFunctionSearch("test_en_schedule_templates_function_search"))
+    filename = gb_filename_prefix+'/ScheduleTemplates.html'
+    fp = file(filename,'wb')
+
+    runner = HTMLTestRunner.HTMLTestRunner(
+            stream=fp,
+            title='Test Result',
+            description='Schedule Templates'
+            )
+ #   runner = unittest.TextTestRunner()
+    runner.run(testsuite)
